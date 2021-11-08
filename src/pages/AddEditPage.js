8705ddb8977378b91a0b4d69e9e2442b2118d885
Ketmuangpak <s.ketmuangpak@accenture.com>
2021-11-08 21:34:11 +0700

    remove some file

--- src/pages/AddEditPage.js
+++ src/pages/AddEditPage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import AddEdit from "../components/addEdit/AddEdit";
-// import AddContact from "../components/addContact/AddContact";
 
 const AddEditPage = () => {
   return (
     <div>
       <AddEdit/>
-      {/* <AddContact /> */}
+      
     </div>
   );
 };
